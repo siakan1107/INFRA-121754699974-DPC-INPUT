67c43b1d6a709edac84fdd0ae49a35db57426827
kibeom <siakan1107@gmail.com>
2023-11-07 17:42:16 +0900

    test

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
-tf
+module "identity_center_dpc" {
+  source  = "siakan1107/INFRA-121754699974-DPC"
+  version = "1.10.4"
+
+  tfc_hostname                = var.tfc_hostname
+  tfc_organization_name       = var.tfc_organization_name
+  tfc_project_name            = var.tfc_project_name
+  dpc_name                    = var.dpc_name
+  vcs_repo_identifier         = var.vcs_repo_identifier
+  vcs_repo_oauth_token_id     = var.vcs_repo_oauth_token_id
+  managed_policies            = var.managed_policies
+  custom_policies             = var.custom_policies
+  tfc_provider_arn            = local.tfc_provider_arn
+  tfc_provider_client_id_list = local.tfc_provider_client_id_list
+
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "tfc_hostname" {
+  type        = string
+  default     = "app.terraform.io"
+  description = "The hostname of the TFC or TFE instance you'd like to use with AWS"
+}
+
+variable "tfc_organization_name" {
+  type        = string
+  default     = "CT_TEST_KB"
+  description = "The name of your Terraform Cloud organization"
+}
+
+variable "tfc_project_name" {
+  type        = string
+  default     = "Testkkb"
+  description = "The name of your Terraform Cloud project"
+}
+
+variable "dpc_name" {
+  type        = string
+  default     = "identity_center"
+  description = "The name of your dpc"
+}
+
+variable "vcs_repo_identifier" {
+  type        = string
+  default     = "siakan1107/infra-121754699974-identitycenter"
+  description = "The name of vcs_repo_identifier"
+}
+
+variable "vcs_repo_oauth_token_id" {
+  type        = string
+  default     = "ot-gZ8WnNTbRFDhkJtn"
+  description = "The name of your vcs_repo_oauth_token_id"
+}
+
+variable "managed_policies" {
+  type        = list(string)
+  default     = ["AdministratorAccess", "IAMFullAccess"]
+  description = "The name of your managed_policie"
+}
+
+variable "custom_policies" {
+  type        = list(string)
+  default     = ["test1", "test2"]
+  description = "The name of your custom_policies"
+}
+
