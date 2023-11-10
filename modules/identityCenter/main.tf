471f9ff487e2a992abfd37182a3d27475f1b8c90
kibeom <siakan1107@gmail.com>
2023-11-10 17:43:37 +0900

    teset

---
+++ modules/identityCenter/main.tf
@@ -0,0 +1,12 @@
+module "identity_center" {
+  source = "github.com/siakan1107/INFRA-121754699974-DPC"
+
+  tfc_hostname                = var.tfc_hostname
+  tfc_organization_name       = var.tfc_organization_name
+  tfc_project_name            = var.tfc_project_name
+  dpc_name                    = var.dpc_name
+  vcs_repo_identifier         = var.vcs_repo_identifier
+  vcs_repo_oauth_token_id     = var.oauth_token_id
+  managed_policies            = var.managed_policies
+  custom_policies       = { test1 = file("policy/test1.json"), test2 = file("policy/test2.json") }
+}
---
+++ modules/main.tf
@@ -0,0 +1,7 @@
+module "main_identity_center" {
+    source = "./identityCenter"
+}
+
+module "main_organization_main" {
+    source = "./organization"
+}
