42bf1c649ccce9aa417326757ab0cc8e50522ee0
kibeom <siakan1107@gmail.com>
2023-11-10 13:11:47 +0900

    test

--- locals.tf
+++ locals.tf
@@ -1,32 +1,19 @@
-locals {
-#   custom_policies  = <<EOF 
-#   {
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Effect" : "Allow",
-#         "Action" : "*",
-#         "Resource" : "*"
-#       }
+# locals {
+#   custom_policies = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "sid" : "test",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "*"
+#             ],
+#             "Resource": [
+#                 "*"
+#             ]
+#         }
 #     ]
-#   }
-#   EOF
-
-  custom_policies = <<EOF
-{
-    "Version": "2012-10-17",
-    "sid" : "test",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "*"
-            ],
-            "Resource": [
-                "*"
-            ]
-        }
-    ]
-}
-EOF
-}
+# }
+# EOF
+# }
