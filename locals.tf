88d357eaf646725b473d72ab682a68cba9b38fdd
kibeom <siakan1107@gmail.com>
2023-11-10 11:39:58 +0900

    test

--- locals.tf
+++ locals.tf
@@ -1,12 +1,31 @@
 locals {
-  custom_policies = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : "*",
-        "Resource" : "*"
-      }
+#   custom_policies  = <<EOF 
+#   {
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Action" : "*",
+#         "Resource" : "*"
+#       }
+#     ]
+#   }
+#   EOF
+
+  custom_policies = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
     ]
-  })
+}
+EOF
 }
