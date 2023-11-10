9982748819eb6a345f49fcea1d5b3fe017edccac
kibeom <siakan1107@gmail.com>
2023-11-10 17:24:31 +0900

    test

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-module "identity_center_dpc" {
-  source = "github.com/siakan1107/INFRA-121754699974-DPC"
-#   version = "1.0.1" 
-
-  tfc_hostname            = var.tfc_hostname
-  tfc_organization_name   = var.tfc_organization_name
-  tfc_project_name        = var.tfc_project_name
-  dpc_name                = var.dpc_name
-  vcs_repo_identifier     = var.vcs_repo_identifier
-  managed_policies        = var.managed_policies
-  custom_policies = {test1 =file("policy/test1.json"), test2=file("policy/test2.json")}
-
+module "dpc" {
+  source = "./modules"
 }
