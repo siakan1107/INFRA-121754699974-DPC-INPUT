c416910e88e42e69d9548cf8aa445eabfd8556bf
kibeom <siakan1107@gmail.com>
2023-11-14 00:13:09 +0900

    test

---
+++ identityCenter.tf
@@ -0,0 +1,14 @@
+module "identityCenter" {
+  source = "github.com/siakan1107/INFRA-121754699974-DPC"
+
+  tfc_hostname          = "app.terraform.io"
+  tfc_organization_name = "CT_TEST_KB"
+  tfc_project_name      = "Testkkb"
+  dpc_name              = "identityCenter"
+  vcs_repo_identifier   = "siakan1107/infra-121754699974-identitycenter"
+  managed_policies      = ["AdministratorAccess", "IAMFullAccess"]
+  custom_policies       = { test1 = file("${path.module}/task/policies/test1.json"), test2 = file("${path.module}/task/policies/test2.json") }
+
+  tfc_provider_arn            = module.openIdConnect.tfc_provider_arn
+  tfc_provider_client_id_list = [module.openIdConnect.tfc_provider_client_id_list]
+}
---
+++ openIdconnect.tf
@@ -0,0 +1,4 @@
+module "openIdConnect" {
+  # source = "github.com/siakan1107/INFRA-121754699974-DPC/modules/openIdConnect"
+  source = "./task/openIdconnect"
+}
