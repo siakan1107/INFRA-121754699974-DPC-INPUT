16155c7341aa0c0d7bcbeed38fbf0d1b5e13dc42
kibeom <siakan1107@gmail.com>
2023-11-14 00:15:56 +0900

    test

--- openIdconnect.tf
+++ openIdconnect.tf
@@ -1,4 +1,4 @@
 module "openIdConnect" {
-  # source = "github.com/siakan1107/INFRA-121754699974-DPC/modules/openIdConnect"
-  source = "./task/openIdconnect"
+  source = "github.com/siakan1107/INFRA-121754699974-DPC/modules/openIdConnect"
+  # source = "./task/openIdconnect"
 }
