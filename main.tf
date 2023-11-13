195becd5fd972652ff5e8875b1896cd79ce81ab9
kibeom <siakan1107@gmail.com>
2023-11-13 22:36:56 +0900

    test

--- main.tf
+++ main.tf
@@ -17,17 +17,17 @@ module "identityCenter" {
   tfc_provider_client_id_list = module.openIdConnect.tfc_provider_client_id_list
 }
 
-module "organizatioin" {
-  source = "github.com/siakan1107/INFRA-121754699974-DPC"
+# module "organizatioin" {
+#   source = "github.com/siakan1107/INFRA-121754699974-DPC"
 
-  tfc_hostname          = "app.terraform.io"
-  tfc_organization_name = "CT_TEST_KB"
-  tfc_project_name      = "Testkkb"
-  dpc_name              = "organization"
-  vcs_repo_identifier   = "siakan1107/infra-121754699974-organization"
-  managed_policies      = ["AWSOrganizationsFullAccess"]
-  custom_policies       = {}
+#   tfc_hostname          = "app.terraform.io"
+#   tfc_organization_name = "CT_TEST_KB"
+#   tfc_project_name      = "Testkkb"
+#   dpc_name              = "organization"
+#   vcs_repo_identifier   = "siakan1107/infra-121754699974-organization"
+#   managed_policies      = ["AWSOrganizationsFullAccess"]
+#   custom_policies       = {}
 
-  tfc_provider_arn            = module.openIdConnect.tfc_provider_arn
-  tfc_provider_client_id_list = module.openIdConnect.tfc_provider_client_id_list
-}
+#   tfc_provider_arn            = module.openIdConnect.tfc_provider_arn
+#   tfc_provider_client_id_list = module.openIdConnect.tfc_provider_client_id_list
+# }
