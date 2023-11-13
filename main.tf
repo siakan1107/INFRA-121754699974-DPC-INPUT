5fcb083a10c10ae1a5e139ee6a4a2b4bfd726988
kibeom <siakan1107@gmail.com>
2023-11-13 22:05:15 +0900

    test

---
+++ main.tf
@@ -0,0 +1,37 @@
+module "openIdConnect" {
+  source = "github.com/siakan1107/INFRA-121754699974-DPC/modules/openIdConnect"
+}
+
+module "identityCenter" {
+  source = "github.com/siakan1107/INFRA-121754699974-DPC"
+
+  depends_on = [module.openIdConnect]
+
+  tfc_hostname          = "app.terraform.io"
+  tfc_organization_name = "CT_TEST_KB"
+  tfc_project_name      = "Testkkb"
+  dpc_name              = "identityCenter"
+  vcs_repo_identifier   = "siakan1107/infra-121754699974-identitycenter"
+  managed_policies      = ["AdministratorAccess", "IAMFullAccess"]
+  custom_policies       = { test1 = file("${path.module}/modules/policies/test1.json"), test2 = file("${path.module}/modules/policies/test2.json") }
+
+  tfc_provider_arn            = module.openIdConnect.tfc_provider_arn
+  tfc_provider_client_id_list = module.openIdConnect.tfc_provider_client_id_list
+}
+
+module "organizatioin" {
+  source = "github.com/siakan1107/INFRA-121754699974-DPC"
+
+  depends_on = [module.openIdConnect]
+
+  tfc_hostname          = "app.terraform.io"
+  tfc_organization_name = "CT_TEST_KB"
+  tfc_project_name      = "Testkkb"
+  dpc_name              = "organization"
+  vcs_repo_identifier   = "siakan1107/infra-121754699974-organization"
+  managed_policies      = ["AWSOrganizationsFullAccess"]
+  custom_policies       = {}
+
+  tfc_provider_arn            = module.openIdConnect.tfc_provider_arn
+  tfc_provider_client_id_list = module.openIdConnect.tfc_provider_client_id_list
+}
